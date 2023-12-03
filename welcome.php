2a8fecfb344d0838b10abb47d8b56b778e556c2f
omar20010 <tu4108641@taibahu.edu.sa>
2023-12-03 22:32:20 +0300

    hello

--- welcome.php
+++ welcome.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
 </head>
-<body>
+<body> 
     <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
     <a href="logout.php">Logout</a>
 </body>
